e650bb2fd788837e8cdf394c18d9efdf17f27cda
VelamuriPrasad <72890831+VelamuriPrasad@users.noreply.github.com>
2020-10-15 02:07:28 +0530

    Update Factors.c

--- c/Factors.c
+++ c/Factors.c
@@ -3,14 +3,14 @@
 int main()
 {
     printf("\n\n\t\tStudytonight - Best place to learn\n\n\n");
-    int  num, i;
+    int  num1, i;
     printf("Enter the number to find the factors of :  ");
-    scanf("%d",&num);
-    printf("\n\n\nFactors of %d are \n\n", num);
+    scanf("%d",&num1);
+    printf("\n\n\nFactors of %d are \n\n", num1);
 
-    for(i = 1; i <= num/2; i++)
+    for(i = 1; i <= num1/2; i++)
     {
-        if(num%i == 0)
+        if(num1%i == 0)
             printf("\t\t\t%d\n", i);
     }
 
