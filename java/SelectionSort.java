c78c494b7101b19f532c255fa88edc3f37fa2610
VelamuriPrasad <72890831+VelamuriPrasad@users.noreply.github.com>
2020-10-15 02:00:59 +0530

    Update SelectionSort.java

--- java/SelectionSort.java
+++ java/SelectionSort.java
@@ -4,11 +4,11 @@ public class SelectionSort {
     
     public static void selectionSorting(int arr[]) {
         
-        int i=0;
+        int i1=0;
         int minIndex=0;
         while(i<arr.length) {                  // for iterating over all the elements
-            int min=arr[i];
-            for(int j=i+1;j<arr.length;j++) {    //for finding minimum among remaining elements which haven't been checked
+            int min=arr[i1];
+            for(int j=i1+1;j<arr.length;j++) {    //for finding minimum among remaining elements which haven't been checked
                     if(arr[j]<min) {
                         min=arr[j];
                     }
@@ -18,13 +18,13 @@ public class SelectionSort {
                 minIndex++;
                 k++;
             }
-            while(minIndex!=i) {     // for shifting remaining elements and making space for placing minimum valued element 
+            while(minIndex!=i1) {     // for shifting remaining elements and making space for placing minimum valued element 
                 arr[minIndex]=arr[minIndex-1];
                 minIndex--;
             }
         minIndex=0;
-        arr[i]=min; // placing minimum valued element on that particular index
-        i++;
+        arr[i1]=min; // placing minimum valued element on that particular index
+        i1++;
         }
         for(int k=0;k<arr.length;k++) {
             System.out.print(arr[k]+ " ");
@@ -39,9 +39,9 @@ public class SelectionSort {
 		System.out.println("enter the size of the array:");
 		int n=sc.nextInt();
 		int[] arr=new int[n];
-		for(int i=0;i<arr.length;i++) {
+		for(int i1=0;i1<arr.length;i1++) {
 			System.out.println("please enter a number:");
-			arr[i]=sc.nextInt();
+			arr[i1]=sc.nextInt();
 		}
 		selectionSorting(arr);
 		
